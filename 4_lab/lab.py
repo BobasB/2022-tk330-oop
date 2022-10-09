75b4c325065b904ca36434c0641b74c1a32a206e
Bohdan <bugil.bogdan@gmail.com>
2022-10-09 13:45:40 +0300

    lab_4 examples

---
+++ 4_lab/lab.py
@@ -0,0 +1,11 @@
+import datetime
+import os
+
+#import pandas as pd
+#
+#df = pd.DataFrame({'num_legs': [2, 4], 'num_wings': [2, 0]}, index=['falcon', 'dog'])
+#print(df)
+#
+#print(datetime.datetime.now())
+
+print(os.environ["VAR_1"], "ми проситали змінну")
